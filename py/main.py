ff9246fa6a71fdcf61c89f2bdd02d56be4bdc23c
Chris Joakim <christopher.joakim@gmail.com>
2019-04-27 11:07:28 -0400

    main.py generating orders and line items

--- py/main.py
+++ py/main.py
@@ -1,6 +1,6 @@
 
 
-# Chris Joakim, Microsoft, 2019/04/25
+# Chris Joakim, Microsoft, 2019/04/27
 
 import csv, json, math, os, random, sys, time, ssl, traceback
 
@@ -13,16 +13,60 @@ getcontext().prec = 6
 
 class Order:
 
-    def __init__(self, id):
-        self.id = id
-        self.fields = list()
+    def __init__(self, order_number, li_count):
+        # Id,Date,StoreNumber,PaymentType,CreditCardNumber,SalesTax,OrderTotal
+        self.fake = Faker()
+        self.id = order_number
+        self.date = '2019/04/27'
+        self.payment_type = 'ccard'
+        self.credit_card = self.fake.credit_card_number(card_type=None)
+        self.tax = Decimal(0)
+        self.total= Decimal(0)
+        self.line_items = list()
+        for line_number in range(1, li_count + 1):
+            self.generate_line_item(line_number)
+
+
+    def generate_line_item(self, line_number):
+        item = LineItem(self.id, line_number, self.fake)
+        self.tax = self.tax + item.tax
+        self.total = self.total + item.total 
+        self.line_items.append(item)
+
+    def as_csv(self):
+        return '{},{},{},{},{},{}'.format(
+            self.id, self.date, self.payment_type, self.credit_card,
+            self.tax, self.total)
+
+    def display(self):
+        print('---')
+        print(self.as_csv())
+        for item in self.line_items:
+            print(item.as_csv())
 
 class LineItem:
 
-    def __init__(self, order_id, id):
-        self.order_id = id
+    def __init__(self, order_id, id, fake):
+        self.line_item_qty_tup = (1,2,3)
+        self.price_tup = (0.99, 1.99, 2.99, 3.99, 4.99, 5.99, 9.99, 19.99, 49.99, 999.99)
+        # Id,OrderId,LineItem,SKU,Quantity,UnitPrice,SalesTax,ItemTotal
+        self.fake = fake
+        self.order_id = order_id
         self.id = id
-        self.fields = list()
+        self.sku = self.fake.license_plate().replace(' ','-').replace('•','-')
+        self.quantity = random.choice(self.line_item_qty_tup)
+        self.unit_price = random.choice(self.price_tup)
+        self.tax = Decimal(0)
+        self.total= Decimal(0)
+        qxp = Decimal(self.quantity * self.unit_price) 
+        tax = qxp * Decimal(0.05).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+        self.tax = Decimal(tax).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+        self.total = Decimal(qxp + tax).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+
+    def as_csv(self):
+        return '{},{},{},{},{},{},{}'.format(
+            self.order_id, self.id, self.sku, self.quantity, 
+            self.unit_price, self.tax, self.total)
 
 class Main:
 
@@ -32,9 +76,7 @@ class Main:
         self.line_item_count_tup = (1,2,3,4,5,6,7,8,1,10)
         self.line_item_qty_tup = (1,2,3)
         self.price_tup = (0.99, 1.99, 2.99, 3.99, 4.99, 5.99, 9.99, 19.99, 49.99, 999.99)
-        self.order_lines = list()
-        self.line_item_lines = list()
-        self.delivery_lines = list()
+        self.orders = list()
 
     def order_fields(self):
         return 'Id,Date,StoreNumber,PaymentType,CreditCardNumber,SalesTax,OrderTotal'.split('.')
@@ -55,22 +97,23 @@ class Main:
 
             elif func == 'csv_to_documents':
                 self.csv_to_documents()
-
             else:
                 print('Error: invalid function: {}'.format(func))
         else:
             print('Error: no function argument provided.')
 
-    def generate_csv_orig(self):
+    def generate_csv(self):
         for order_number in range(1,11):
             li_count = random.choice(self.line_item_count_tup)
             print('order_number: {} li_count: {}'.format(order_number, li_count))
-            for line_item_number in range(1, li_count+1):
-                self.generate_random_line_item(order_number, line_item_number)
-
+            self.generate_order(order_number, li_count)
+            
+    def generate_order(self, order_number, li_count):
+        order = Order(order_number, li_count)
+        self.orders.append(order)
+        order.display()
 
     def generate_csv_orig(self):
-
         date = arrow.utcnow().format('YYYY-MM-DD')
         curr_order_num, curr_order = -1, list()
         curr_order_tax, curr_order_tot = Decimal(0), Decimal(0)
